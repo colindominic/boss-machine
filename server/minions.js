93e8b9e1a6692aa1b1cb7d6bcccbc9296ac08e1b
colindominic <32608479+colindominic@users.noreply.github.com>
2018-07-21 15:12:49 -0700

    get all minions

--- server/minions.js
+++ server/minions.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
+import getAllFromDatabase from '/db.js';
+
 const minionsRouter = express.Router();
+
+const minions = [];
+minions = getAllFromDatabase('minions');
+
+//Get all Minions
+minionsRouter.get('/', (req, res, next) => {
+  res.send(minions);
+});
 
 module.exports = minionsRouter;
