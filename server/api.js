4eb9aee59943ac58238c024757363e9411f628f6
colindominic <32608479+colindominic@users.noreply.github.com>
2018-07-21 12:06:12 -0700

    api routers set-up

--- server/api.js
+++ server/api.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+//Minions Router
+const minionsRouter = require('./minions.js');
+app.use('/minions', minionsRouter);
 
+//Ideas Router
+const ideasRouter = require('./ideas.js');
+app.use('/ideas', ideasRouter);
+
+//Meetings Router
+const meetingsRouter = require('./meetings.js');
+app.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
